e4bf8f1b14595a18c544c252a4a9e7905991909b
Mustakim-Khan <mustakimzidan@gmail.com>
2022-03-31 20:34:04 +0200

    User

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,26 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\ToDoList;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     private int $numberOfUsers = 10;
 
     public function load(ObjectManager $manager): void
     {
         $faker = \Faker\Factory::create();
+        $todolist = $manager->getRepository(ToDoList::class)->findAll();
+
         for ($i = 0; $i < $this->numberOfUsers; $i++) {
             $object = (new User())
                 ->setFirstname($faker->firstName)
                 ->setLastname($faker->lastName)
                 ->setAge($faker->numberBetween(7, 100))
                 ->setEmail($faker->email)
-                ->setPassword($faker->password(8, 40));
+                ->setPassword($faker->password(8, 40))
+                ->setTodolist($faker->randomElement($todolist));
             $manager->persist($object);
         }
         $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            TodolistFixture::class
+        ];
     }
 }
