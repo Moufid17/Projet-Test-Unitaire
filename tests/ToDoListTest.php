bd6d215fd3360e7f9f82600b2e607720371e5e7d
Moufid17 <moufid.mtr@gmail.com>
2022-04-01 19:07:41 +0200

    ToDoList Test: 	- Redefine Item new params (name, content) 	- Define <testAddElevenItem()> with his criteria.

--- tests/ToDoListTest.php
+++ tests/ToDoListTest.php
@@ -15,32 +15,48 @@ class ToDoListTest extends TestCase
         $this->todolist = new ToDoList();
         parent::setUp();
     }
-
+    
     public function testFirstOneItem()
     {
-        $item1 = new Item();
+        $item1 = new Item("Item1_name","Item1_content");
         $this->todolist->addItem($item1);
         $this->assertEquals(1, $this->todolist->getItems()->count());
     }
 
     public function testSecondItemValidInterval()
     {
-        $item1 = new Item();
-        sleep(7);
-        $item2 = new Item();
+        $item1 = new Item("Item1_name","Item1_content");
+        sleep(2);
+        $item2 = new Item("Item2_name","Item2_content");
         $this->todolist->addItem($item1);
         $this->todolist->addItem($item2);
+        // dd($tab);
+        // $tab = [$item1, $item2];
         $this->assertEquals(2, $this->todolist->getItems()->count());
     }
 
     public function testSecondItemInvalidInterval()
     {
-        $item1 = new Item();
-        sleep(4);
-        $item2 = new Item();
+        $item1 = new Item("Item1_name","Item1_content");
+        sleep(1);
+        $item2 = new Item("Item2_name","Item2_content");
         $this->todolist->addItem($item1);
         $this->todolist->addItem($item2);
         $this->assertNotEquals(2, $this->todolist->getItems()->count());
+    }
+
+    public function testAddElevenItem()
+    {
+        #Add 10 Items
+        $itemCollection = [];
+        for($i = 0; $i < 10; $i++) {
+            $this->todolist->addItem(new Item("Item_name" . strval($i),"Item_content" . strval($i)));
+            sleep(2);
+        }
+        
+        // dd($this->todolist->getItems()->count());
+        $this->todolist->addItem(new Item("Item11_name","Item11_content"));
+        $this->assertEquals(10, $this->todolist->getItems()->count());
     }
 
 }
