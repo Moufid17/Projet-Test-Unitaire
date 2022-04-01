4558aa96fa1b57f05318eca28eb8a301a0d9b759
Moufid17 <moufid.mtr@gmail.com>
2022-04-01 19:10:16 +0200

    Item Test : 	- Define Item test with for assertions: 		- Empty name or content case 		- Same name or content case 		- Change new name or content

---
+++ tests/ItemTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Entity\Item;
+use App\Entity\ToDoList;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ItemTest extends TestCase
+{
+    private Item $item;
+
+    protected function setUp(): void
+    {
+        $this->item = new Item("Item1_name","Item1_content");
+        //User
+        $user = new User("daniel@gmail.com","Daniel", "Larssonsdqsd", "passwordTest", 24);
+        // todolist
+        $todolist = new ToDoList();
+        $todolist->setOwner($user); // Set Owner
+        $todolist->addItem($this->item); // Set ToDoList
+        parent::setUp();
+    }
+
+    public function testValidDueToNameEmptyIsNotConsidered()
+    {
+        $this->item->setName("");
+        // dd($this->item); // Voir l'item. Aucun changement de nom n'est effectuer.
+        $this->assertNotEmpty($this->item->getName());
+    }
+
+    public function testValidDueToContentEmptyIsNotConsidered()
+    {
+        $this->item->setContent("");
+        // dd($this->item); // Voir l'item. Aucun changement de content n'est effectuer.
+        $this->assertNotEmpty($this->item->getContent());
+    }
+
+    public function testValidDueToNameIsNotEmpty()
+    {
+        $this->item->setName("Item1_name_new");
+        // dd($this->item); // Voir l'item. Le nom est a été changé.
+        $this->assertNotEmpty($this->item->getName());
+    }
+
+    public function testValidDueToContentIsNotEmpty()
+    {
+        $this->item->setContent("Item1_content_new");
+        // dd($this->item); // Voir l'item. Le content a été changé.
+        $this->assertNotEmpty($this->item->getContent());
+    }
+}
